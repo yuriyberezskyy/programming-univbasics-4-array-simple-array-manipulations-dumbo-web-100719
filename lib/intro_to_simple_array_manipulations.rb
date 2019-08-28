f62f4549c83b03ba730c5d1046ae211c70e9faed
Yuriy Berezskyy <bereza95@gmail.com>
2019-08-28 05:36:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,9 @@ def using_push(array,string)
   updated_array = array.push(string)
 end
   
-  def using_unshift(array,string)
+def using_unshift(array,string)
     array.unshift(string)
-  end
+end
+
+def using_pop(array)
+  array.pop()
