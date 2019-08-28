80092e300744c5c3b6619954880dfc02d05b4b4c
Yuriy Berezskyy <bereza95@gmail.com>
2019-08-28 06:04:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,5 +26,6 @@ def using_concat(array1,array2)
    array1.concat(array2)
 end
 
-def using_insert(array,elemen)
-  array.insert()
+def using_insert(array,element)
+  array.insert(4) = element
+end
